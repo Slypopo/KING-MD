186367b5fd400c3997d494f71688ed5c33acf4eb
Slypopo <anaaba059@gmail.com>
2024-06-15 16:58:03 +0000

    Update config.js

--- config.js
+++ config.js
@@ -8,23 +8,23 @@ global.mongodb = process.env.MONGODB_URI || "mongodb+srv://mohsin:mohsin@cluster
 global.port= process.env.PORT || 5000
 global.email = 'textnow1pak@gmail.com'
 global.github = 'https://github.com/naveeddogar/KING-MD'
-global.location = 'pakistan/multan'
+global.location = 'ghana/ghana'
 global.gurl = 'https://whatsapp.com/channel/0029Va66s2IJENxvTJjUtM1w' // add your username
 global.sudo = process.env.SUDO || '233596780685'
 global.devs = '233596780685';
 global.website = 'https://king-md-session.onrender.com/' // Hello world
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/5541dfe4dc8c2f51e3f02.jpg'
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/23998e60fad313cd316fc.jpg'
 module.exports = {
   botname:   process.env.BOT_NAME === undefined ? 'ODS TECHNOLOGIES' : process.env.BOT_NAME,
   ownername: process.env.OWNER_NAME === undefined ? 'ODS TECHNOLOGIES' : process.env.OWNER_NAME,
   sessionName:  process.env.SESSION_ID === undefined ? true : process.env.SESSION_ID,
-  author:  process.env.PACK_AUTHER.split("ODS TECHNOLOGIES")[0] === undefined ? 'Naveed-Dogar' : process.env.PACK_AUTHER.split(";")[0],
+  author:  process.env.PACK_AUTHER.split("ODS TECHNOLOGIES")[0] === undefined ? 'ODS TECHNOLOGIES' : process.env.PACK_AUTHER.split(";")[0],
   auto_read_status :  process.env.AUTO_READ_STATUS  || 'false' ,
-  packname:  process.env.PACK_NAME.split("ODS TECHNOLOGIES")[1] === undefined ? 'King-Md' : process.env.PACK_NAME.split(";")[1],
+  packname:  process.env.PACK_NAME.split("ODS TECHNOLOGIES")[1] === undefined ? 'ODS TECHNOLOGIES' : process.env.PACK_NAME.split(";")[1],
   autoreaction:  process.env.AUTO_REACTION ||  'true' ,
   antibadword :  process.env.ANTI_BAD_WORD === undefined ? '' : process.env.ANTI_BAD_WORD,
   alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
-  antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '212' : process.env.FAKE_COUNTRY_CODE,
+  antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '' : process.env.FAKE_COUNTRY_CODE,
   readmessage:  process.env.READ_MESSAGE === undefined ? false : process.env.READ_MESSAGE,
   auto_status_saver: process.env.AUTO_STATUS_SAVER === undefined ? false : process.env.AUTO_STATUS_SAVER,
   HANDLERS:  process.env.PREFIX === undefined ? '.' : process.env.PREFIX,
@@ -44,7 +44,7 @@ module.exports = {
     APP_NAME: process.env.HEROKU_APP_NAME === undefined ? 'amme' : process.env.HEROKU_APP_NAME
 },
   VERSION: process.env.VERSION === undefined ? 'Ｖ-1.3.0' : process.env.VERSION,
-  LANG: process.env.THEME|| 'KING-MD',
+  LANG: process.env.THEME|| 'ODS TECHNOLOGIES',
   WORKTYPE: process.env.WORKTYPE === undefined ? 'public' : process.env.WORKTYPE
 };
 
